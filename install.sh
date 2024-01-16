bbca609e1ab9a4ebc46572dd0dccfc9b1361790c
gutoiualexandru <alexandru.gutoiu@yahoo.con>
2024-01-16 17:47:14 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pip install qiskit
+pip install pytket
+pip install pytket-quantinuum
+pip install pylatexenc
+pip install cirq
+pip install pytket
+pip install pytket-qiskit
+pip install pytket-cirq
+pip install qiskit.providers.ibmq
+pip install qiskit-ibmq-provider
